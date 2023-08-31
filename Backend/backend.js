e8473be3134f8f3c0951530f3563fb6486bc305b
Jasa95 <jaco3020@stud.kea.dk>
2023-08-31 10:11:59 +0200

    backend test

--- Backend/backend.js
+++ Backend/backend.js
@@ -1,12 +1,23 @@
 import Express from "express";
 const app = Express();
-const port = 6969
+const port = 6969;
 
 app.listen(port, () => {
-console.log(`Server started on ${port}`);
+  console.log(`Server started on ${port}`);
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello welcome to my website")
+  res.send("Hello welcome to my website");
 });
 
+app.post("/", (req, res) => {
+  res.send("A POST requested");
+});
+
+app.put("/put", (req, res) => {
+  res.send("PUT request on /user");
+});
+
+app.delete("/delete", (req, res) => {
+  res.send("Delete request on /delete");
+});
